f7ce49b4c288f075d799606dea80fedc7bf3b16b
saisai.wu <saisai.wu@funpluspartners.com>
2023-11-10 19:35:10 +0800

    feat: node

--- node-koa/config/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * @description 默认配置文件
- */
-module.exports = {
-  localhostDB: "mongodb://127.0.0.1:27017/hb",
-  //  公网数据库地址
-  databasePro: "mongodb://root:123456@110.110.110.110:27017/hb", // mongodb://用户名:密码@服务器公网IP:端口/库的名称
-};
--- vite-template/src/views/Home/index.tsx
+++ vite-template/src/views/Home/index.tsx
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
-import { Button } from 'antd';
+import { Button, Upload } from 'antd';
 import axios from 'axios';
 const Home = () => {
-  console.log(import.meta.env);
+  const [selectedFile, setSelectedFile] = useState<any>(null);
+  // console.log(import.meta.env);
   const handleOnclick = () => {
-    axios.get('http://127.0.0.1:3000/info').then((res) => {
-      console.log(res);
-    });
+    console.log(selectedFile);
+
+    const formData = new FormData();
+    formData.append('userId', '2222');
+    formData.append('file', selectedFile);
+    axios
+      .post('http://127.0.0.1:3000/upload', formData, {
+        // headers: {
+        //   'Content-Type': 'multipart/form-data',
+        // },
+      })
+      .then((res) => {
+        console.log(res);
+      });
   };
+
+  // const handlePictureChange = (e: any) => {
+  //   setSelectedFile(e.target.files[0]);
+  // };
+  // const uploadChangeHandler = (e: any) => {
+  //   console.log(e);
+  // };
   return (
     <>
       Home
+      <Upload
+        name='file'
+        customRequest={handleOnclick}
+        beforeUpload={(e: any) => {
+          setSelectedFile(e);
+        }}>
+        <Button>Click to Upload</Button>
+      </Upload>
+      {/* <input type='file' onChange={handlePictureChange} /> */}
       <Button onClick={handleOnclick}>测试后台</Button>
       <progress value='22' max='100'></progress>
       <Outlet />
